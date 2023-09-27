de0661c2b41dfb231f06b92201e7fa2038fcc5ad
Heithem Tarchouna <75651342+HeithemTarchouna@users.noreply.github.com>
2023-09-27 12:13:55 +0200

    added comments to MultiHead AttentionBlock

--- multiHeadAttentionBlock.py
+++ multiHeadAttentionBlock.py
@@ -2,7 +2,31 @@ import torch
 import torch.nn as nn
 
 class MultiHeadAttentionBlock(nn.Module):
+    
     def __init__(self, d_model:int, h:int, dropout:float):
+
+        """
+        MultiHeadAttentionBlock Module:
+
+        This class represents the multi-head attention block used within the Transformer's encoder and decoder layers.
+        The multi-head mechanism allows the model to focus on different words for a given input word. 
+
+        Parameters:
+        - d_model (int): The dimensionality of input and output. Represents depth of the input feature set.
+        - h (int): Number of attention heads.
+        - dropout (float): Dropout rate for regularization.
+        
+        Attributes:
+        - d_model (int): The dimensionality of input and output.
+        - h (int): Number of attention heads.
+        - d_k (int): Dimensionality of queries and keys (d_model // h).
+        - w_q (nn.Linear): Linear layer for transforming queries.
+        - w_k (nn.Linear): Linear layer for transforming keys.
+        - w_v (nn.Linear): Linear layer for transforming values.
+        - w_o (nn.Linear): Final linear layer for producing the output.
+        - dropout (nn.Dropout): Dropout layer for regularization.
+        """
+
         super().__init__()
         self.d_model = d_model
         self.h = h 
@@ -16,8 +40,23 @@ class MultiHeadAttentionBlock(nn.Module):
 
     @staticmethod   
     def attention(query,key,value,mask,droput:nn.Dropout):
+        """
+        Static method for calculating attention weights and scores.
+        
+        Parameters:
+        - query (torch.Tensor): Query tensor of shape (batch_size, h, seq_len, d_k).
+        - key (torch.Tensor): Key tensor of shape (batch_size, h, seq_len, d_k).
+        - value (torch.Tensor): Value tensor of shape (batch_size, h, seq_len, d_k).
+        - mask (torch.Tensor): Mask to avoid attending to certain positions.
+        - dropout (nn.Dropout): Dropout layer for regularization.
+        
+        Returns:
+        - Tuple[Torch.Tensor]: Tuple containing output tensor and attention scores.
+        """
+
         d_k = query.shape[-1]
 
+        # Compute attention scores using scaled dot-product attention mechanism
         # (batch_size, h, seq_len, d_k) @ (batch_size, h, d_k, seq_len) -> (batch_size, h, seq_len, seq_len)
         attention_scores = query @ key.transpose(-2,-1) / torch.sqrt(torch.tensor(d_k, dtype=torch.float32))
 
@@ -27,7 +66,10 @@ class MultiHeadAttentionBlock(nn.Module):
             attention_scores = attention_scores.masked_fill(mask==0, -1e9)
 
         # (batch_size, h, seq_len, seq_len,seq_len)
+        # Apply softmax to obtain attention distribution
         attention_scores = nn.Softmax(dim=-1)(attention_scores)
+
+        # Apply dropout for regularization
         if droput is not None:
             attention_scores = droput(attention_scores)
         
