e47670f1cbb8c2ca73397d955bde807090176132
Heithem Tarchouna <75651342+HeithemTarchouna@users.noreply.github.com>
2023-10-03 12:28:12 +0200

    Added comments to transformer class

--- transformer.py
+++ transformer.py
@@ -9,49 +9,59 @@ from multiHeadAttentionBlock import MultiHeadAttentionBlock
 from feedForwardBlock import FeedForwardBlock
 from inputEmbeddings import InputEmbeddings
 
-
 class Transformer(nn.Module):
-    
-    def __init__(self,
-                encoder:Encoder,
-                decoder:Decoder,
-                src_embedding:InputEmbeddings,
-                tgt_embedding:InputEmbeddings,
-                src_positional_encoding:PositionalEncoding,
-                tgt_positional_encoding:PositionalEncoding,
-                projection_layer:ProjectionLayer
-                ):
-        super().__init__()
-        self.encoder = encoder
-        self.decoder = decoder
-        self.src_embedding = src_embedding
-        self.tgt_embedding = tgt_embedding
-        self.src_positional_encoding = src_positional_encoding
-        self.tgt_positional_encoding = tgt_positional_encoding
-        self.projection_layer = projection_layer
-
-
-
-    def encode(self,src,src_mask):
+    """
+    Transformer Module:
+    ...
+    Methods:
+    - encode(src: Tensor, src_mask: Tensor) -> Tensor:
+        src: Tensor of shape (batch_size, src_seq_len)
+        src_mask: Tensor of shape (batch_size, 1, 1, src_seq_len)
+        Returns: Tensor of shape (batch_size, src_seq_len, d_model)
+        
+    - decode(tgt: Tensor, encoder_output: Tensor, src_mask: Tensor, tgt_mask: Tensor) -> Tensor:
+        tgt: Tensor of shape (batch_size, tgt_seq_len)
+        encoder_output: Tensor of shape (batch_size, src_seq_len, d_model)
+        src_mask: Tensor of shape (batch_size, 1, 1, src_seq_len)
+        tgt_mask: Tensor of shape (batch_size, 1, tgt_seq_len, tgt_seq_len)
+        Returns: Tensor of shape (batch_size, tgt_seq_len, d_model)
+        
+    - project(tgt: Tensor) -> Tensor:
+        tgt: Tensor of shape (batch_size, tgt_seq_len, d_model)
+        Returns: Tensor of shape (batch_size, tgt_seq_len, vocab_size)
+    ...
+    """
+    def encode(self, src, src_mask):
+        """
+        Perform encoding on source sequence.
+        src: Tensor of shape (batch_size, src_seq_len)
+        src_mask: Tensor of shape (batch_size, 1, 1, src_seq_len)
+        Returns: Tensor of shape (batch_size, src_seq_len, d_model)
+        """
         src = self.src_embedding(src)
         src = self.src_positional_encoding(src)
-        src = self.encoder(src,src_mask)
-        return src
+        return self.encoder(src, src_mask)
 
-        
-
-    def decode(self,tgt,encoder_output,src_mask,tgt_mask):
+    def decode(self, tgt, encoder_output, src_mask, tgt_mask):
+        """
+        Perform decoding using encoder outputs and target sequence.
+        tgt: Tensor of shape (batch_size, tgt_seq_len)
+        encoder_output: Tensor of shape (batch_size, src_seq_len, d_model)
+        src_mask: Tensor of shape (batch_size, 1, 1, src_seq_len)
+        tgt_mask: Tensor of shape (batch_size, 1, tgt_seq_len, tgt_seq_len)
+        Returns: Tensor of shape (batch_size, tgt_seq_len, d_model)
+        """
         tgt = self.tgt_embedding(tgt)
         tgt = self.tgt_positional_encoding(tgt)
-        tgt = self.decoder(tgt,encoder_output,src_mask,tgt_mask)
-        return tgt
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
 
-    def project(self,tgt):
+    def project(self, tgt):
+        """
+        Project the decoder output to the size of the target vocabulary.
+        tgt: Tensor of shape (batch_size, tgt_seq_len, d_model)
+        Returns: Tensor of shape (batch_size, tgt_seq_len, vocab_size)
+        """
         return self.projection_layer(tgt)
-    
-
-
-
 def build_transformer(src_vocab_size:int,
                       tgt_vocab_size:int,
                       src_seq_len:int,
@@ -62,6 +72,13 @@ def build_transformer(src_vocab_size:int,
                       num_decoder_layers:int=6,
                       dropout:float=0.1,
                       d_ff:int=2048):
+    """
+    Function to build the Transformer model.
+    
+    Given the hyperparameters, this function initializes all necessary components and returns a Transformer model.
+    Additionally, it initializes the weights of the model with Xavier Uniform Initialization for better convergence.
+    """
+    
     # Encoder
     #---------------------------------------------------------------------------------------
     src_embedding = InputEmbeddings(d_model,src_vocab_size)
